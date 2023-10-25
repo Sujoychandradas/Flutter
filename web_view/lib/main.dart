907d498b9697f8843f34b2231ceb0ec0ba1ba037
Sujoy Chandra Das <sujoy.chandradas@gmail.com>
2023-10-25 20:26:06 +0600

    Web View flutter

---
+++ web_view/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // final controller = WebViewController();
+  // loadStore() {
+  //   controller
+  //     ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  //     ..setBackgroundColor(Colors.black)
+  //     ..loadRequest(Uri.parse('https://www.google.com'));
+  // }
+  final controller = WebViewController();
+  loadstore() {
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    loadstore();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Webview')),
+        body: WebViewWidget(
+          controller: controller,
+        ));
+  }
+}
